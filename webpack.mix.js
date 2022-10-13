44a19aec9728556cffac9d09d04c0f6226361f88
mizuki <km0925q@gmail.com>
2022-10-13 12:12:27 +0900

    jqueryを導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/login.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .autoload({
+        "jquery": ['$', 'window.jQuery'],
+      })
     .sourceMaps();
