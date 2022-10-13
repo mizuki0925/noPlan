04d355b543e3d08a3dd142bcbd7d9e1de777cc23
mizuki <km0925q@gmail.com>
2022-10-13 19:07:54 +0900

    アクセス記録を追加

---
+++ app/Models/AccessLog.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class AccessLog extends Model
+{
+    protected $table = 'accessLogs';
+    use HasFactory;
+
+    public function set($request)
+    {
+        try {
+            $log = new AccessLog();
+            // ユーザID
+            $log->userId = (Auth::check()) ? Auth::id() : null;
+            // 内容
+            $log->body = $request->fullUrl();
+            // 実行クエリ(実装方法を考える)
+            $log->query = null;
+            // ユーザエージェント
+            $log->userAgent = $request->userAgent();
+            // IPアドレス(ロードバランサを考えたときは改善が必要)
+            $log->ip = $request->ip();
+
+            $log->save();
+        } catch (\Exception $e) {
+            Log::error($e);
+        }
+    }
+}
